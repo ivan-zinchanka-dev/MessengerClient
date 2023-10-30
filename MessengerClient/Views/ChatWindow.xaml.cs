90fd3f807b3d404c55f0f4d648272a07363c8f42
JanZinch <jan.zinchanka@mail.ru>
2023-10-30 15:57:40 +0300

    refactor: list view

--- MessengerClient/Views/ChatWindow.xaml.cs
+++ MessengerClient/Views/ChatWindow.xaml.cs
@@ -1,11 +1,42 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using MessengerClient.Core.Models;
 
 namespace MessengerClient.Views;
 
 public partial class ChatWindow : Window
 {
+    private ObservableCollection<Message> _messages = new ObservableCollection<Message>();
+
     public ChatWindow()
     {
         InitializeComponent();
+
+        _messages.Add(new Message()
+        {
+            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        });
+
+        _messages.Add(new Message()
+        {
+            Text = "Who?"
+        });
+        
+        _messages.Add(new Message()
+        {
+            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        });
+        
+        _messages.Add(new Message()
+        {
+            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        });
+        
+        _messages.Add(new Message()
+        {
+            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        });
+        
+        _messagesListView.ItemsSource = _messages;
     }
 }
