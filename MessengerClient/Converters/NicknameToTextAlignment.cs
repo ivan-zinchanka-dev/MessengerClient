0d50185cbc08bb4f7537ef95ba014b7a3bac875f
JanZinch <jan.zinchanka@mail.ru>
2023-11-05 17:17:07 +0300

    refactor

---
+++ MessengerClient/Converters/NicknameToTextAlignment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MessengerClient.Converters;
+
+public class NicknameToTextAlignment : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string nickname)
+        {
+            return nickname == App.Instance.CurrentUser.Nickname ? TextAlignment.Right : TextAlignment.Left;
+        }
+        else
+        {
+            return TextAlignment.Center;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
