8e1bf9e512bca331acece70a93b3467c8ade8894
JanZinch <jan.zinchanka@mail.ru>
2023-11-05 18:56:20 +0300

    fix: alignment

---
+++ MessengerClient/Converters/NicknameToHorizontalAlignment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MessengerClient.Converters;
+
+public class NicknameToHorizontalAlignment : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string nickname)
+        {
+            return nickname == App.Instance.CurrentUser.Nickname ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+        }
+        else
+        {
+            throw new InvalidOperationException("Value must be a System.String");
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is HorizontalAlignment textAlignment && textAlignment == HorizontalAlignment.Right)
+        {
+            return App.Instance.CurrentUser.Nickname;
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
+}
